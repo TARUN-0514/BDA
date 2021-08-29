10aa65a3a7a9afada3465286e3e4bbfacf169538
adithyaGHegde <hegdeaditya39@gmail.com>
2021-08-29 12:26:55 +0530

    Put everything in a function to be used in mainmenu

--- BD_PRO.py
+++ BD_PRO.py
@@ -1,6 +1,21 @@
 def reg():
     print("FOLLOW THE STEPS FOR THE FURTHER REGISTRATION :")
+    name = input("ENTER THE NAME OF THE DONOR : ")
+    get_age()
+    
+    contact = int(input("ENTER THE CONTACT NUMBER OF THE DONOR : "))
+    address = input("ENTER THE ADDRESS OF THE DONOR :")
 
+    blood_group()
+    
+    print("\n\nGO THROUGH THE FOLLOWING QUESTIONAIRE TO COMPLETE THE APPLICATION PROCESS :(Answer in yes or no)")
+    Q1=input("\n1.Have you taken any painkillers, anti-inflammatories or aspirin (including Ecotrin)?\n2.Have you had a cold, flu, sore throat, fever, infection, open wound or allergies?\n3.Have you had an immunisation or vaccination?\n4.Have you taken part in a drug trial, vaccine trial, or clinical research?\n5.Heart (e.g. stents), lung or circulatory problems (eg. clots) or a bleeding disorder?\n6.Cancer, skin cancer (melanoma, basal cell carcinoma, squamous cell carcinoma) or leukaemia\n7.Diabetes, asthma, tuberculosis (TB) or kidney disease\n8.Any other serious illnesses, severe allergic reactions, tropical diseases or used medication not mentioned above\n9.Has your doctor advised you to donate blood to treat a medical condition such as high iron, ‘thick blood’,polycythaemia, haemochromatosis\n")
+    if Q1 == ("YES"):
+      print("SORRY YOUR APPLICATION IS REJECTED")
+    elif Q1 == ("NO"):
+      print("YOUR APPLICATION HAS BEEN CONSIDERED PLEASE GO THORUOGH USER LOGIN FOR REGISTRATION")    
+    else:
+      print("PLEASE ENTER YES OR NO ")
 
 class donor:
     def getData(self, name, age, blood_group, contact, address):
@@ -9,9 +24,6 @@ class donor:
         donor.blood_group=blood_group
         donor.contact = contact
         donor.address = address
-
-
-name = input("ENTER THE NAME OF THE DONOR : ")
 
 
 def get_age():
@@ -27,24 +39,10 @@ def get_age():
         except ValueError:
             print("Amount must be a number, try again")
     return checker
-get_age()
-
-contact = int(input("ENTER THE CONTACT OF THE DONOR : "))
-address = input("ENTER THE ADDRESS OF THE DONOR :")
-
 
 def blood_group():
         print("\nCHOOSE NO. ACCORDING TO YOUR BLOOD GROUP : ")
-        choice1=input("\n1.A+ \t2.B+ \t3.O+ \t4.AB+ \t5.A- \t6.B- \t7.O- \t8.AB-")
-        print("Blood Group Chosen")
+        choice1=input("\n1.A+ \t2.B+ \t3.O+ \t4.AB+ \t5.A- \t6.B- \t7.O- \t8.AB-\n")
+        print("\nBlood Group Chosen")
 
 
-print("GO THROUGH THE FOLLOWING QUESTIONAIRE TO COMPLETE THE APPLICATION PROCESS :(Answer in yes or no)")
-
-Q1=input("1.Have you taken any painkillers, anti-inflammatories or aspirin (including Ecotrin)?\n2.Have you had a cold, flu, sore throat, fever, infection, open wound or allergies?\n3.Have you had an immunisation or vaccination?\n4.Have you taken part in a drug trial, vaccine trial, or clinical research?\n5.Heart (e.g. stents), lung or circulatory problems (eg. clots) or a bleeding disorder?\n6.Cancer, skin cancer (melanoma, basal cell carcinoma, squamous cell carcinoma) or leukaemia\n7.Diabetes, asthma, tuberculosis (TB) or kidney disease\n8.Any other serious illnesses, severe allergic reactions, tropical diseases or used medication not mentioned above\n9.Has your doctor advised you to donate blood to treat a medical condition such as high iron, ‘thick blood’,polycythaemia, haemochromatosis\n")
-if Q1 == ("YES"):
-  print("SORRY YOUR APPLICATION IS REJECTED")
-elif Q1 == ("NO"):
-  print("YOUR APPLICATION HAS BEEN CONSIDERED PLEASE GO THORUOGH USER LOGIN FOR REGISTRATION")    
-else:
-  print("PLEASE ENTER YES OR NO ")
